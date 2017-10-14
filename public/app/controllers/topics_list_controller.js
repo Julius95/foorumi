6ca8d470ce9d60da3a95ed8e8488fd30e2f403f5
Julius <juliuskoljonen@gmail.com>
2017-10-14 16:51:03 +0000

    turha printti poistettu

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -3,10 +3,6 @@ FoorumApp.controller('TopicsListController', function($scope, $location, Api){
         console.log(data)
         $scope.topics = data
     })
-    Api.getTopic(6)
-    .success((data, status, headers, config) => {
-        console.log(data)
-    })
     
     $scope.addNewTopic = ((topic) => {
       Api.addTopic(topic)
